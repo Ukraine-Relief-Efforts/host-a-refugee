5ff5cb8f2336706dd61cf3dc94565d6bbbeb4c45
Seth McCullough <seth@endevrs.dev>
2022-02-27 12:37:01 -0500

    Remove dotenv dependency

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,2 @@
-import { config } from 'dotenv';
-
-config();
-
 export const AIRTABLE_URL = `https://api.airtable.com/v0/${process.env.AIRTABLE_APP_ID}/Hosts?maxRecords=50&view=Grid%20view`;
 export const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;

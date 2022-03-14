4cf0ce7349690ea841fcdf0bacdd464dfbdd5a17
Ben Paget <17719054+MyElectricSheep@users.noreply.github.com>
2022-03-14 11:41:40 +0000

    Increase of map height on main page

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -13,7 +13,7 @@ const Map = ({ pins, pin }: MapProps) => {
       zoom={pin ? 9 : 6}
       scrollWheelZoom={false}
       style={{
-        height: 400,
+        height: pin ? 400 : 600,
         width: '100%',
         zIndex: '0',
         borderRadius: '0rem 0rem 0.5rem 0.5rem',
